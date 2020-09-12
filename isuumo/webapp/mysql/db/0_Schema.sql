5e9e73f94ab657d6a4942693e4f352929613002d
yyamada12 <12yacropolisy@gmail.com>
2020-09-12 06:11:47 +0000

    add index

--- isuumo/webapp/mysql/db/0_Schema.sql
+++ isuumo/webapp/mysql/db/0_Schema.sql
@@ -17,7 +17,10 @@ CREATE TABLE isuumo.estate
     door_height INTEGER             NOT NULL,
     door_width  INTEGER             NOT NULL,
     features    VARCHAR(64)         NOT NULL,
-    popularity  INTEGER             NOT NULL
+    popularity  INTEGER             NOT NULL,
+    index idx_rent (rent),
+    index idx_door_height (door_height),
+    index idx_door_width (door_width)
 );
 
 CREATE TABLE isuumo.chair
@@ -34,5 +37,9 @@ CREATE TABLE isuumo.chair
     features    VARCHAR(64)     NOT NULL,
     kind        VARCHAR(64)     NOT NULL,
     popularity  INTEGER         NOT NULL,
-    stock       INTEGER         NOT NULL
+    stock       INTEGER         NOT NULL,
+    index idx_price (price),
+    index idx_height (height),
+    index idx_width (width),
+    index idx_depth (depth)
 );
